4a221bf9fec076d127d150a18c02f81d9bdd7bbd
Akilap11 <watlankathilaka@students.nsbm.ac.lk>
2023-06-09 00:53:09 +0530

    u

--- frontend/mean-stack-agency/src/app/app.component.ts
+++ frontend/mean-stack-agency/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-stack-agency';
+
+  title = 'Mean Stack Agency';
+
+  nums=[10,20];
+  sum=()=>{
+    return this.nums[0]+this.nums[1];
+  }
+  red="color:red;"
+  green="color:green;"
+
+  message="";
+  clickMe=(val: string)=>{
+    console.log(val);
+    this.message=val;
+  }
+
 }
